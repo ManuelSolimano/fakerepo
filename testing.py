099ae630a7826a3ece35c4cd892d030e48f985f7
Manuel Solimano <masolimano@uc.cl>
2018-11-17 16:24:04 -0300

    Added wavelet method to iterate_method function. Warning: this haven't been tested.

--- testing.py
+++ testing.py
@@ -3,15 +3,22 @@ import detection_tools as dt
 import imageio
 import glob
 
-def iterate_method(method_name='ela', **kwargs):
+def iterate_method(method_name='ela', root_path, **kwargs):
+    """ Use this function to iterate through all target images
+    in a given root directory. Every detection thecnique added
+    to this function must return a single output image.
+    """
 
     if method_name == 'ela':
         method = dt.ela_substract
 
+    elif method_name == 'wavelet':
+        method = dt.noise_detection_wavelet
+
     else:
         return
 
-    path_list = glob.glob('images/*')
+    path_list = glob.glob('{:s}/*'.format(root_path.strip('/'))
 
     for image_path in path_list:
         result = method(image_path, **kwargs)
@@ -23,4 +30,5 @@ def iterate_method(method_name='ela', **kwargs):
 
 
 if __name__ == "__main__":
+    #Change this if you want to use another method.
     iterate_method(method_name='ela', quality=85)
